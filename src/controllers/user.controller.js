2dcb3b4504616a788f7573f202dd8a44c4528986
Satyam Jha <satyamforyou7@gmail.com>
2026-03-19 01:42:22 +0530

    updation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,20 @@
-import { asynHandler.js } from ".. /"
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-const registerUser = asynHandler( async (req , res) => {
-    return res.status(200).json({
-        message : "ok"
-    })
-})
+const registerUser = asyncHandler(async (req, res) => {
+    //let us have a look at the steps whuch we are going to take as whole 
+    //Get user detail from frontnend
+    //validation - not empty
+    //check for images , check for avatar
+    //we will upload that to cloudinary 
+    //create user object - create entry in db
+    //remove password and refresh  token field from rsponsde
+    //check for user cretion
+    //return response
+    // return res.status(200).json({
+    //     message: "This is satyam you mf , im coming for you all"    });
 
- export {registerUser}
+    const {fullName, email, username, password} = req.body
+    console.log("emails : ", emails)
+});
+
+export { registerUser };
--- src/index.js
+++ src/index.js
@@ -4,26 +4,19 @@ import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-
 dotenv.config({
     path: './env'
-}) 
+})
 
 connectDB()
-
 .then(() => {
-    app.listen(process.env.PORT || 8000, ()=> {
-        console.log('the server is running  at port : ${process.env.PORT}');
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`The server is running at port : ${process.env.PORT}`);
     })
 })
 .catch((error) => {
-console.log("error occured", error);
+    console.log("error occured", error);
 })
-
-
-
-
-
 
 
 
