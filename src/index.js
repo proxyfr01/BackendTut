f96b4737902e7c59ea3cda9303f65d1a694463cc
Satyam Jha <satyamforyou7@gmail.com>
2026-03-24 02:16:38 +0530

    updation

--- src/index.js
+++ src/index.js
@@ -45,25 +45,25 @@ import app from "./app.js";
 })();
 */
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import {app} from './app.js'
+import { app } from "./app.js";
+
 dotenv.config({
     path: './.env'
-})
+});
 
-
+const PORT = process.env.PORT || 8000;
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
+    app.listen(PORT, "0.0.0.0", () => {
+        console.log(`⚙️ Server is running at port : ${PORT}`);
+    });
 })
 .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
-})
-
+});
 
 
 
