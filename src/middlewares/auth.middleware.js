e482fb465dd14c40ab2831a4236c18d180608bf2
Satyam Jha <satyamforyou7@gmail.com>
2026-04-01 00:37:44 +0530

    updation

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,52 @@
-import { asyncHandler } from "../utils/asynchandler";
+// import { asyncHandler } from "../utils/asynchandler";
 
-export const verifyJWT = asyncHandler(async(req, res, next) => {
-    req.cookies?.accessToken || req.header
-    ("Authorization")?
+import { application } from "express";
+import multer from "multer";
+
+// export const verifyJWT = asyncHandler(async(req, res, next) => {
+//     req.cookies?.accessToken || req.header
+//     ("Authorization")?
+// })
+
+const loggingMiddleware=  function ( req, res, next) {
+    console.log("this is logging middlewre");
+    next();
+}
+
+
+const authMiddleware = function ( req, res ,next){
+    console.log( " this is the second middle ware i orderto authrntuivcte");
+    next();
+}
+//next basically here shows or indicates that there should be aniothet 
+//middleware coming in the wayu
+const validationmIDDLEWARE = function (req, res, next){
+    console.log(" this should be the last  middle ware in the way")
+}
+
+application.use(loggingMiddleware);
+application.use(authMiddleware)
+application.use(validationmIDDLEWARE);
+
+application.get("/", (req, res) => {
+    console.log(req.body);
+
+    res.send("this is th backend routet calling");
+
 })
+
+application.listen(port, () => {
+    console.log("the website is gettig displayed at the port 3000")
+})
+
+//if we want to make a user speecific routr as in only allwoing access to th 
+//specified acess users only;
+//the below is the syntax for that :
+
+Router.get("/students", authMiddleware,multer ,(req, res) => {
+
+})
+
+
+//
+
